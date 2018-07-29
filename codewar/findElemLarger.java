c619d3e64d9d0e916c1998010b9665f2a4f23edb
Leo Liu <leo.liu@enmasse.com.au>
2018-07-29 21:48:40 +1000

    Added codewar/findElemLarger.java.

--- codewar/findElemLarger.java
+++ codewar/findElemLarger.java
@@ -4,16 +4,17 @@ import com.codewar.lib.BinarySearch;
 import com.codewar.lib.MergeSort;
 import com.codewar.lib.Printer;
 
-public class ServianCompetition {
+public class findElemLarger {
 	public static void main(String[] args) {
 		int[] ar = {6, 1, 7, 8, 2};
 		int[] br = {0, 3, 4, 7};
 		int[] result = new int[br.length];
 		
-//		To sort the ar first
+//		To sort the ar first Time complexity: O(nlogn)
 		MergeSort.sort(ar, 0, ar.length - 1);
 		
 //		To find the index of the first number that is larger than br[index]
+//		Time complexity: O(mlogn)
 		for (int i = 0; i < br.length; i++) {
 			int num = br[i];
 			
